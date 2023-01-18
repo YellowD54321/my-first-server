3795081e013bbeb30242c630c709ad0483687f4b
allen <yellowd54321@gmail.com>
2023-01-18 15:45:59 +0800

    新增兩支tappay的測試用API：pay-by-prime和pay-by-card-token

--- app.js
+++ app.js
@@ -15,6 +15,16 @@ const cors = require("cors");
 //   });
 // });
 
+const partnerKey =
+  "partner_MisOlKdtyn9TOvWhASjNMyTKTLtvwsMZv77ZwcdTBant9T6lKBf0a9SD";
+
+const merchantId = "AllenHuang_CTBC";
+
+const cardSecret = {
+  cardToken: "",
+  cardKey: "",
+};
+
 app.use(cors("http://localhost:3000"));
 
 app.use(bodyParser.json());
@@ -25,38 +35,86 @@ app.use(
 );
 // app.use("/", express.static(__dirname + "html"));
 
-// app.post("pay-by-prime", (req, res, next) => {
-//   // code
-//   const post_data = {
-//     prime: req.body.prime,
-//     partner_key:
-//       "partner_6ID1DoDlaPrfHw6HBZsULfTYtDmWs0q0ZZGKMBpp4YICWBxgK97eK3RM",
-//     merchant_id: "GlobalTesting_CTBC",
-//     amount: 1,
-//     currency: "TWD",
-//     details: "An apple and a pen.",
-//     cardholder: {
-//       phone_number: "+886923456789",
-//       name: "jack",
-//       email: "example@gmail.com",
-//     },
-//     remember: false,
-//   };
+app.post("/pay-by-prime", (req, res, next) => {
+  const prime = req.body.prime;
 
-//   axios
-//     .post("https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime", post_data, {
-//       headers: {
-//         "x-api-key":
-//           "partner_6ID1DoDlaPrfHw6HBZsULfTYtDmWs0q0ZZGKMBpp4YICWBxgK97eK3RM",
-//       },
-//     })
-//     .then((response) => {
-//       console.log(response.data);
-//       return res.json({
-//         result: response.data,
-//       });
-//     });
-// });
+  // code
+  const post_data = {
+    prime: prime,
+    partner_key: partnerKey,
+    merchant_id: merchantId,
+    amount: 1,
+    currency: "TWD",
+    details: "An apple and a pen.",
+    cardholder: {
+      phone_number: "+886923456789",
+      name: "王小明",
+      email: "LittleMing@Wang.com",
+      zip_code: "100",
+      address: "台北市天龍區芝麻街1號1樓",
+      national_id: "A123456789",
+    },
+    remember: true,
+  };
+
+  axios
+    .post("https://sandbox.tappaysdk.com/tpc/payment/pay-by-prime", post_data, {
+      headers: {
+        "x-api-key": partnerKey,
+      },
+    })
+    .then((response) => {
+      console.log("pay-by-prime response", response.data);
+      const data = response.data;
+      const status = data.status;
+      if (status !== 0) {
+        return res.json({
+          result: data,
+        });
+      }
+      const hasCardSecret = data.hasOwnProperty("card_secret");
+      if (hasCardSecret) {
+        cardSecret.cardToken = data.card_secret.card_token;
+        cardSecret.cardKey = data.card_secret.card_key;
+      }
+      return res.json({
+        result: data,
+      });
+    });
+});
+
+app.post("/pay-by-card-token", (req, res, next) => {
+  const url = "https://sandbox.tappaysdk.com/tpc/payment/pay-by-token";
+
+  const postHeader = {
+    "Content-Type": "application/json",
+    "x-api-key": partnerKey,
+  };
+
+  const postData = {
+    card_key: cardSecret.cardKey,
+    card_token: cardSecret.cardToken,
+    partner_key: partnerKey,
+    currency: "TWD",
+    merchant_id: merchantId,
+    details: "TapPay Test",
+    amount: 100,
+  };
+
+  axios
+    .post(url, postData, {
+      headers: postHeader,
+    })
+    .then((response) => {
+      console.log(response.data);
+      const data = response.data;
+      const status = data.status;
+      console.log("data", data);
+      return res.json({
+        result: data,
+      });
+    });
+});
 
 app.get("/user/allen", (req, res) => {
   console.log("get api is called");
